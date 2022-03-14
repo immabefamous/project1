578ee4c2a558c2716cde07ab31b94a74dba508af
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-14 14:34:45 -0400

    added js event

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 alert('JavaScript is running!')
+
+let btn = document.getElementById('btn')
+// noSpaceInTheWord with a capital letter every word is camel case
+btn.addEventListener('click', () => {
+    alert('Clicked!');
+}
